4c208d608e1fca2e2d25731c0262f7f38a699624
007Igorek <134934432+007Igorek@users.noreply.github.com>
2023-11-29 17:05:04 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,14 @@
-import java.sql.SQLOutput;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
-
     public static void main(String[] args) throws InterruptedException {
         List<Thread> threadList = new ArrayList<>();
+
         String[] texts = new String[25];
         for (int i = 0; i < texts.length; i++) {
-            texts[i] = generateText("abb", 30_000);
+            texts[i] = generateText("aab", 30_000);
         }
         long startTs = System.currentTimeMillis();
         for (String text : texts) {
@@ -18,8 +19,9 @@ public class Main {
         }
         for (Thread thread : threadList) {
             thread.join();
-         }
+        }
         long endTs = System.currentTimeMillis();
+
         System.out.println("Time: " + (endTs - startTs) + "ms");
     }
     public static String generateText(String letters, int length) {
@@ -31,32 +33,30 @@ public class Main {
         return text.toString();
     }
     public static Thread getNewThread(String text) {
-    return new Thread(
-            ()-> {
-                int maxSize = 0;
-                for (int i = 0; i < text.length(); i++) {
-                    for (int j = 0; j < text.length(); j++)
-                        if (i >= j) {
-                            continue;
-                        }
-                    boolean bFound = false;
-
-                    for (int k = i; k < j; k++) {
-                        if (text.charAt(k) == 'b') {
-                            bFound = true;
-                            break;
+        return new Thread(
+                () -> {
+                    int maxSize = 0;
+                    for (int i = 0; i < text.length(); i++) {
+                        for (int j = 0; j < text.length(); j++) {
+                            if (i >= j) {
+                                continue;
+                            }
+                            boolean bFound = false;
+                            for (int k = i; k < j; k++) {
+                                if (text.charAt(k) == 'b') {
+                                    bFound = true;
+                                    break;
+                                }
+                            }
+                            if (!bFound && maxSize < j - i) {
+                                maxSize = j - i;
+                            }
                         }
                     }
-                    if (!bFound && maxSize < j - i) {
-                        maxSize = j - i;
-                    }
+                    System.out.println(text.substring(0, 100) + " -> " + maxSize);
                 }
-            }
-            System.out.println(text.substring(0, 100) + " -> " + maxSize);
+        );
     }
-);
-            }
 }
-
 
 
